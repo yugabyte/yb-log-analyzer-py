aed520330c6b7082a1aed90c263edc9362f8f0a6
Yogesh Jadhav <pgyogesh@outlook.com>
2023-03-25 10:48:45 +0530

    Fixed #5
    Added SST files limit exceeded

--- analyzer_dict.py
+++ analyzer_dict.py
@@ -15,16 +15,17 @@ regex_patterns = {
     "Rejecting Write request: Soft memory limit exceeded": r"Soft memory limit exceeded",
     "Number of aborted transactions not cleaned up on account of reaching size limits": r"Number of aborted transactions not cleaned up on account of reaching size limits",
     "Long wait for safe op id": r"Long wait for safe op id",
-    "Sample log message": r"Sample log message",
+    "SST files limit exceeded": r"SST files limit exceeded",
     # Add more log messages here
 }
 solutions = {
     "Rejecting Write request: Soft memory limit exceeded": """ This typically means that we have overloaded system.
-    Check this KB for more details: https://support.yugabyte.com/hc/en-us/articles/4403688844045-Throttling-mechanism-in-YugaByte-TServer-due-to-high-Memory-Usage-""",
+    KB for more details: https://support.yugabyte.com/hc/en-us/articles/4403688844045-Throttling-mechanism-in-YugaByte-TServer-due-to-high-Memory-Usage-""",
     "Number of aborted transactions not cleaned up on account of reaching size limits": """This typically means that we need to run compaction on offending tablets
     Check this case for more details
     https://yugabyte.zendesk.com/agent/tickets/5416""",
     "Long wait for safe op id": """This means that Write on disk is slow. This could be because of slow disk or load on the system.""",
-    "Sample log message": "Solution for Sample log message error",
+    "SST files limit exceeded": """This means that we have too many SST files. This could be because of slow compaction or load on the system.
+    KB for more details:https://support.yugabyte.com/hc/en-us/articles/4491328438413-Writes-failed-with-error-SST-files-limit-exceeded-"""
     # Add more solutions here
 }
